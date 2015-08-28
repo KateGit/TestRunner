6d0971ce6748aee7a8dd92dd5a2f6bf49ad9d4c6
Kate Milrud <kmilrud@yahoo.com>
2015-08-27 22:20:59 -0700

    Initial commit

---
+++ run.bat
@@ -0,0 +1,51 @@
+@ECHO OFF
+::===================================
+SET GITHUB_ACCOUNT=KateGit
+SET WS_DIR=Workspace
+SET REPO_NAME=Title_Validation_CSV
+SET APP_VERSION=1.1
+SET MAIN_CLASS=core.Title_Validation_CSV  
+SET ARGS_01=
+::=========================================
+
+
+
+::=========================================
+
+IF "%JAVA_HOME%" == "" GOTO EXIT_JAVA
+ECHO Java installed
+IF "%M2%" == "" GOTO EXIT_MVN
+ECHO Maven installed
+CALL git --version > nul 2>&1
+IF NOT %ERRORLEVEL% == 0 GOTO EXIT_GIT
+ECHO Git installed
+
+GOTO NEXT
+
+:NEXT
+IF NOT EXIST C:\%WS_DIR% GOTO NO_WORKSPACE
+IF EXIST C:\%WS_DIR%\%REPO_NAME% RMDIR /S /Q C:\%WS_DIR%\%REPO_NAME%
+CD C:\%WS_DIR%
+git clone https://github.com/%GITHUB_ACCOUNT%/%REPO_NAME%.git
+CD %REPO_NAME%
+SLEEP 2
+CALL mvn package -Dbuild.version="%APP_VERSION%"
+ECHO.
+ECHO Executing Java programm ...
+java -cp C:\%WS_DIR%\%REPO_NAME%\target\%REPO_NAME%-%APP_VERSION%-jar-with-dependencies.jar %MAIN_CLASS% %ARG_01%
+GOTO END
+
+:EXIT_JAVA
+ECHO No Java installed
+GOTO END
+:EXIT_MVN
+ECHO No Maven installed
+GOTO END
+:EXIT_GIT
+ECHO No Git installed
+GOTO END
+:NO_WORKSPACE
+ECHO %WS_DIR% does not exist
+GOTO END
+:END
+CD\
